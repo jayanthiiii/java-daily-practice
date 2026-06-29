30e002ac4df67a4d7be0b367a43d99d17ff9a377
Jayanthi K <jayanthikofficial@gmail.com>
2026-06-29 20:20:40 +0530

    Add files via upload

---
+++ IsOdd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class IsEven
+{
+    public static void main(String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<5; i++)
+        {
+            System.out.println("Enter a number");
+            int num = sc.nextInt();
+            if(num % 2 != 0)
+            {
+                System.out.println("Yes");
+            }
+            else
+            {
+                System.out.println("No");
+            }
+        }
+       
+    }
+}
