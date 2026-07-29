de263f345e96994c5dbb5db7570372ea65b8502d
Jayanthi K <jayanthikofficial@gmail.com>
2026-07-29 23:45:42 +0530

    Add files via upload

---
+++ SecondLastDigit.java
@@ -0,0 +1,8 @@
+class SecondLastDigit{
+public static void main(String[] args){
+int num = 532;
+int n = num/10;
+int a = n%10;
+System.out.print(a + " second last digit of the number given " );
+}
+}
